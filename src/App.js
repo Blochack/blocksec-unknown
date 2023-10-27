635532728f387a851c8c38a7f7628873582a5a26
bigchiano <bigchiano@protonmail.com>
2023-10-27 20:10:21 +0300

    using help lib

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
-import { Sankey } from 'react-vis';
-import { fetchTransactions, toEth } from './lib';
+import { fetchTransactions, toEth } from './libs';
+// import {select, selectAll} from "d3";
+// import { ForceGraph } from './components/forceGraph';
+import data from './data/data.json';
+import Tree from 'react-d3-tree';
 
 const nodes = [];
 const links = []
-const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
+const colors = [
+  '#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
   '#E6B333', '#3366E6', '#999966', '#99FF99', '#B34D4D',
   '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A',
   '#FF99E6', '#CCFF1A', '#FF1A66', '#E6331A', '#33FFCC',
@@ -14,7 +18,8 @@ const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
   '#E666B3', '#33991A', '#CC9999', '#B3B31A', '#00E680',
   '#4D8066', '#809980', '#E6FF80', '#1AFF33', '#999933',
   '#FF3380', '#CCCC00', '#66E64D', '#4D80CC', '#9900B3',
-  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'];
+  '#E64D66', '#4DB380', '#FF4D4D', '#99E6E6', '#6666FF'
+];
 
 /* 
   - each node should represent eth being transfered at a transaction and links to represent 
@@ -40,6 +45,7 @@ const colors = ['#FF6633', '#FFB399', '#FF33FF', '#FFFF99', '#00B3E6',
 
 function App() {
   const [txs, setTxs] = useState([])
+  const customData = {}
 
   const fetchTxs = async () => {
     const res = await fetchTransactions("0x629e7Da20197a5429d30da36E77d06CdF796b71A")
@@ -50,33 +56,35 @@ function App() {
     fetchTxs()
   }, [])
 
-  for (let index = 0; index < txs.length; index++) {
-    const tx = txs[index]
-    const name = `${toEth(tx.value)}eth`
-    console.log(toEth(tx.value));
-    nodes.push({ name, color: colors[index] })
+  useEffect(() => {
+    customData.name = `${toEth(txs[0]?.value)}eth`
+    const children = []
+    for (let index = 1; index < txs.length; index++) {
+      const tx = txs[index]
+      const data = {}
+      const name = tx.from.split('')
+      const sentEth = (tx.value)
+      const attr = `${toEth(sentEth)}eth`
 
-    const source = index === 0 ? index : index - 1
-    const target = index === 19 ? 1 : index + 1
-    const value = Math.floor(Math.random() * 80);
-    links.push({ source, target, value, color: colors[index] })
-  }
+      data.name = name
+      if (parseInt(sentEth) > 0)
+        data.attributes = {
+          'Sent Eth': attr
+        }
+      // console.log(toEth(tx.value));
+      // nodes.push({ name, color: colors[index] })
+
+      // const source = index === 0 ? index : index - 1
+      // const target = index === 19 ? 1 : index + 1
+      // const value = Math.floor(Math.random() * 80);
+      // links.push({ source, target, value, color: colors[index] })
+
+    }
+  }, [txs])
 
   return (
-    <div className="App-header">
-      <Sankey
-        nodes={nodes}
-        links={links}
-        width={1200}
-        height={200}
-        onValueClick={(datapoint, event) => {
-        }}
-        onValueMouseOver={(datapoint, event) => {
-        }}
-        onLinkClick={(linkdata, event) => {
-        }}
-      >
-      </Sankey>
+    <div className="App-header" style={{ width: '100%', height: '100vh' }}>
+      <Tree data={data} />
     </div>
   );
 }
