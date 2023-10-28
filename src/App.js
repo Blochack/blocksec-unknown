a6553be3f4bc08e0bbbcb63b3a6437d400e04b67
bigchiano <bigchiano@protonmail.com>
2023-10-28 04:33:33 +0300

    fetch transaction trace

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { fetchTransactions, pickFirstAndLastFourChars, toEth } from './libs';
+import { createChild, fetchTransactions, pickFirstAndLastFourChars, toEth } from './libs';
 // import data from './data/data.json';
 import Tree from 'react-d3-tree';
 
@@ -9,7 +9,7 @@ function App() {
   const [customData, setCustomData] = useState({})
 
   const fetchTxs = async () => {
-    const res = await fetchTransactions("0x629e7Da20197a5429d30da36E77d06CdF796b71A")
+    const res = await fetchTransactions("0x629e7Da20197a5429d30da36E77d06CdF796b71A", 8)
     setTxs(res)
   }
 
@@ -18,28 +18,49 @@ function App() {
   }, [])
 
   useEffect(() => {
-    if (txs.length == 0) return
+    if (txs.length === 0) return
     const cData = {}
+    const sentEth = (txs[0]?.value)
+    const attr = `${toEth(sentEth)}eth`
     cData.name = pickFirstAndLastFourChars(txs[0]?.from_address)
-    cData.attributes = { Sent: `${toEth(txs[0]?.value)}eth` }
-    const children = []
-    for (let index = 1; index < txs.length; index++) {
-      const tx = txs[index]
-      const data = {}
-      const sentEth = (tx.value)
-      const attr = `${toEth(sentEth)}eth`
-
-      data.name = pickFirstAndLastFourChars(tx.from_address)
-      if (parseInt(sentEth) > 0) {
-        data.attributes = {
-          'Sent': attr
-        }
+    if (parseInt(sentEth) > 0) {
+      cData.attributes = {
+        'Sent': attr
       }
-
-      children.push(data)
     }
-    cData.children = children
-    setCustomData(cData);
+    const children = []
+    let index = 1
+    let stillChecking = true
+    const interval = setInterval(() => {
+      if (!stillChecking) return
+      const child = createChild(txs[index])
+      fetchTransactions(txs[index].from_address, 4).then((res) => {
+        child.children = []
+        let j = 0;
+
+        const intervalId = setInterval(() => {
+          if (j < res.length) {
+            child.children.push(createChild(res[j]));
+            j++;
+          } else {
+            clearInterval(intervalId); // Stop the interval when all items are processed
+          }
+        }, 1000);
+      })
+
+      children.push(child)
+      index += 1
+
+      if (index >= txs.length) {
+        cData.children = children
+        setCustomData(cData);
+        stillChecking = false
+      }
+    }, 1000);
+
+    console.log(stillChecking);
+    return () => clearInterval(interval);
+    // eslint-disable-next-line
   }, [txs])
 
   return (
--- src/libs/index.js
+++ src/libs/index.js
@@ -1,8 +1,8 @@
 
-module.exports.fetchTransactions = async (address) => {
+module.exports.fetchTransactions = async (address, count) => {
     let url = `https://api.chainbase.online/v1/account/txs?`
     url += `&chain_id=1&address=${address}&from_block=0`
-    url += `&page=1&limit=20`
+    url += `&page=1&limit=${count}`
 
     const headers = {
         'x-api-key': '2XLh8wLw6X7KMW5DhD28r2HgHr1'
@@ -15,16 +15,31 @@ module.exports.fetchTransactions = async (address) => {
 }
 
 module.exports.toEth = (amount) => {
-    return (parseInt(amount) / 1e18).toLocaleString()
+    return (parseInt(amount) / 1e18)
 }
 
 module.exports.pickFirstAndLastFourChars = (inputString) => {
     if (inputString.length < 8) {
-      return "The string is too short to pick the first and last 4 characters.";
+        return "The string is too short to pick the first and last 4 characters.";
     }
-  
+
     const firstFour = inputString.substring(0, 4);
     const lastFour = inputString.substring(inputString.length - 4);
-  
+
     return `${firstFour}...${lastFour}`
-  }
+}
+
+module.exports.createChild = (tx) => {
+    const data = {}
+    const sentEth = (tx.value)
+    const attr = `${this.toEth(sentEth)}eth`
+
+    data.name = this.pickFirstAndLastFourChars(tx.from_address)
+    if (parseInt(sentEth) > 0) {
+      data.attributes = {
+        'Sent': attr
+      }
+    }
+
+    return data;
+}
